550adbfa9c42604e0cca43b937a035a5da7ef1ba
Djair Alves <djair.dj@gmail.com>
2022-10-08 13:41:45 -0300

    Removendo @Requestbody

--- src/main/java/com/dio/santander/bankline/api/controller/CorrentistaController.java
+++ src/main/java/com/dio/santander/bankline/api/controller/CorrentistaController.java
@@ -5,7 +5,10 @@ import com.dio.santander.bankline.api.model.Correntista;
 import com.dio.santander.bankline.api.repository.CorrentistaRepository;
 import com.dio.santander.bankline.api.service.CorrentistaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -24,7 +27,7 @@ public class CorrentistaController {
     }
 
     @PostMapping
-    public void save(@RequestBody NovoCorrentista correntista) {
+    public void save(NovoCorrentista correntista) {
         service.save(correntista);
     }
 }
