67a05f950a9929eed96ec5249bfbc62eb4f5782c
Djair Alves <djair.dj@gmail.com>
2022-10-08 13:47:10 -0300

    Repondo o @Requestbody

--- src/main/java/com/dio/santander/bankline/api/controller/CorrentistaController.java
+++ src/main/java/com/dio/santander/bankline/api/controller/CorrentistaController.java
@@ -5,10 +5,7 @@ import com.dio.santander.bankline.api.model.Correntista;
 import com.dio.santander.bankline.api.repository.CorrentistaRepository;
 import com.dio.santander.bankline.api.service.CorrentistaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,7 +24,7 @@ public class CorrentistaController {
     }
 
     @PostMapping
-    public void save(NovoCorrentista correntista) {
+    public void save(@RequestBody NovoCorrentista correntista) {
         service.save(correntista);
     }
 }
